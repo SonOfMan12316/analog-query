d40805ce4f6e3fb8ee2f1f49150c3d446ed25e9f
CHARLES EMANYO <charles.emanyo@saed.dev>
2024-06-13 10:13:25 +0100

    setting up

---
+++ analog.js
@@ -0,0 +1,13 @@
+
+// import Keyring from "@polkadot/keyring";
+// import { keygen } from "@analog-labs/timegraph-js";
+
+// // init polkadot keypair for node env
+// const keyring = new Keyring({ type: "sr25519" });
+// const keypair = keyring.add...(... );
+// //Pass signer and address to get keygen instance
+// const _keygen = new keygen({ signer: keypair.sign, address: keypair.address });
+// // generate API key, i.e. role is optional default will be developer
+// const apiKey = await _keygen.createApiKey( < "Role" > );
+// // generate session key
+// const sessionKey = await _keygen.createSessionkey( < "Expiration in ms ");
